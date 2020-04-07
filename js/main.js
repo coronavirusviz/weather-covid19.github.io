3772fdf7b164d5ae5c534a4e816a3aa873cf1b52
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-04-07 18:07:34 +0200

    adding forecast

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ function getCurrentWeather(lat, lon){
          * @param {Object} response 
          */
         success: function (response) {
-            console.log(response);
+
             $("#weather-description").html(response.weather[0].description);
             $("#current-temperature").html(response.main.temp + " C");
             $("#current-wind-speed").html(response.wind.speed + " m/s");
@@ -62,6 +62,7 @@ function getCurrentWeather(lat, lon){
             var iconurl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
 
             $("#weather-icon").attr('src', iconurl);
+            getForecast(lat, lon);
    },
 
            
@@ -79,6 +80,92 @@ function getCurrentWeather(lat, lon){
         }
         
     });
+}
+
+function getForecast(lat, lon){
+    const APPID = "b432b286b3d8beba074a2045c82ee21c";
+   
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=${APPID}&units=metric`,
+        dataType: "JSON",
+     
+        /**
+         * @param {Object} response 
+         */
+        success: function (response) {
+            console.log(response);
+
+            // check if everything is OK
+            if(response.cod == 200){
+                
+                let labels = [];
+                let data = [];
+                let bckColor = [];
+
+                let list = response.list;
+                for (let i = 0; i < response.cnt/4; i++) {
+
+                    data.push(list[i].main.temp);
+                    bckColor.push('rgba(216, 100,84, 1)');
+                    // Converts to datetime
+                    var date = new Date(list[i].dt * 1000);
+                    console.log(date.getHours());
+                    const days = [
+                        'Sun',
+                        'Mon',
+                        'Tue',
+                        'Wed',
+                        'Thu',
+                        'Fri',
+                        'Sat'
+                    ];
+                    labels.push(days[date.getDay()] + " " + date.getHours()+":00");
+                    
+                
+                }
+
+                drawChart(labels, data, bckColor);
+
+            }
+   },
+    error: function(request, status, message){
+        
+    }
+        
+    });
+}
+
+function drawChart(labels, data, bckColor){
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Temperature',
+                data: data,
+                backgroundColor:bckColor,
+                borderWidth: 0,
+                pointBackgroundColor: 'rgba(166, 166, 166, 0.9)'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    },
+                    drawBorder: false
+
+                }],
+                xAxes:[{
+                    drawOnChartArea: true,
+                    drawBorder: false
+                }]
+            }
+        }
+    });
+  
 }
 
 function getCountryName(lat, lon){
@@ -125,7 +212,6 @@ function getCountryName(lat, lon){
  */
 function getCovidInformation(country){
     
-    console.log(country.toLowerCase());
   
     $.ajax({
         url:`https://api.covid19api.com/total/country/${country.toLowerCase()}/status/confirmed`,
@@ -135,8 +221,6 @@ function getCovidInformation(country){
           
           // Get the last case 
           let corona = response[response.length - 1];
-
-          console.log(corona);
 
           $("#corona-location").html(corona.Country); 
           $("#corona-date").html(corona.Date.substring(0, corona.Date.indexOf('T'))); 
